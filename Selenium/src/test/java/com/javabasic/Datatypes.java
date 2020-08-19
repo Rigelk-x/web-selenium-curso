119d44c3bb0de19a35a1a51da68f35a2c5096675
Rigelk-x <rigelk.avila@gmail.com>
2020-08-18 20:34:03 -0500

    Second comimit

---
+++ Selenium/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,35 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		/*
+		 * Datos Numericos 
+		 */
+		
+//		byte x;
+//		short y;
+//		int z;
+//		long w;
+//		
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		w=924555444687L;
+//				
+//			
+//		
+//		System.out.println(x);
+		float base;
+		double altura;
+		
+		base = 30.5F;
+		altura = 40.76; 
+		
+		System.out.println(base);
+
+	}
+
+}
